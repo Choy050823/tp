3fdae879d04b82737f81483f68044231fec8f52a
henghengyh <yihenglee6@gmail.com>
2026-03-11 17:34:45 +0800

    Split the list test into 2 test cases

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -83,12 +83,16 @@ public class AddressBookParserTest {
     }
 
     @Test
-    public void parseCommand_list() throws Exception {
-        assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD) instanceof ListCommand);
+    public void parseCommand_list_invalid() throws Exception {
         assertThrows(ParseException.class,
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE), () ->
                         parser.parseCommand(ListCommand.COMMAND_WORD + " 3")
         );
+    }
+
+    @Test
+    public void parseCommand_list_valid() throws Exception {
+        assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD) instanceof ListCommand);
         assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD + " sort") instanceof ListCommand);
     }
 
