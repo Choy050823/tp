5f15762c857d67fe3f53143cf2b9c7c5b1dc2e17
henghengyh <yihenglee6@gmail.com>
2026-03-11 15:19:25 +0800

    Edit ListCommand.java to support sorting

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -3,7 +3,10 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import java.util.Comparator;
+
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
 
 /**
  * Lists all persons in the address book to the user.
@@ -14,11 +17,27 @@ public class ListCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Listed all persons";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Lists all persons in the address book.\n"
+            + "Parameters: [sort]\n"
+            + "Example: " + COMMAND_WORD + " sort";
+
+    private final boolean shouldSort;
+
+    public ListCommand(boolean shouldSort) {
+        this.shouldSort = shouldSort;
+    }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+        if (shouldSort) {
+            model.getFilteredPersonList()
+                    .sort(Comparator.comparing(person -> person.getName().fullName));
+        }
+
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
